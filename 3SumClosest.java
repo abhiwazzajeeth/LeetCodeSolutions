217e942cf2b4f4cca22012dabd32ca95bf59266b
abhiwazzajeeth <abhijeeth.d@gmail.com>
2020-08-07 22:12:48 -0400

    Update 3SumClosest.java

--- 3SumClosest.java
+++ 3SumClosest.java
@@ -1,31 +1,36 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int closestSum = nums[0] + nums[1] + nums[nums.length - 1];
+        int closestSum = 0;
         
         Arrays.sort(nums);
         
+        if (nums.length <= 3) {
+            for (int num:nums) 
+                closestSum += num; 
+            return closestSum; 
+        }
+        
+        closestSum = nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3];
+        
         for(int i = 0; i < nums.length - 2; i++) {
+            
             int startPointer = i + 1;
             int endPointer = nums.length - 1;
-           
+            
             while(startPointer < endPointer) {
-                
-                int calcSum =  nums[endPointer] + nums[startPointer] + nums[i];
+                int calcSum = nums[i] + nums[startPointer] + nums[endPointer];
                 
                 if(calcSum < target) {
                     startPointer++;
-                }    
-                 
-                else  {
-                    endPointer--;
-                } 
-                                
-                if(Math.abs(calcSum - target) < Math.abs(closestSum - target)) {
-                        closestSum = calcSum;
                 }
- 
+                
+                else {
+                    endPointer--;
+                }
+                if(Math.abs(calcSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = calcSum ;
+                }
             }
-            
         }
         return closestSum;
     }
