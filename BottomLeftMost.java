c068fb45cdbda14daadab185c0b15669b46ebec0
abhiwazzajeeth <abhijeeth.d@gmail.com>
2020-08-09 17:03:15 -0400

    Update BottomLeftMost.java

--- BottomLeftMost.java
+++ BottomLeftMost.java
@@ -15,29 +15,20 @@
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
-        int leftMost = root.val;
         Queue<TreeNode> queue = new ArrayDeque<>();
         
         queue.add(root);
         
         while(!queue.isEmpty()){
-            int size = queue.size();
-            int level = queue.size();
-
-            while(level > 0) {
-                TreeNode current = queue.poll();
-                if(level == size ) {
-                    leftMost = current.val;
-                }                
-                if(current.left != null) {
-                    queue.add(current.left);
-                }
-                if(current.right != null) {
-                    queue.add(current.right);
-                }
-                level--;
+            root = queue.poll();
+            if(root.right != null) {
+                queue.add(root.right);
             }
+            if(root.left != null) {
+                queue.add(root.left);
+            }
+                
         }
-        return leftMost;
+        return root.val;
     }
 }
