bf059f5cbc56fcf4ecf126ae52155fd081a6e2f6
abhiwazzajeeth <abhijeeth.d@gmail.com>
2020-08-10 03:37:15 -0400

    Create JewelsAndStones.java

---
+++ JewelsAndStones.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for(char s : S.toCharArray()) {
+            if(J.indexOf(s) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
